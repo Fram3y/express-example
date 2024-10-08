6a5e7aec571863bd2bc60c691787251ecb94bbfe
Sam Downey <n00212512@iadt.ie>
2024-10-08 10:55:40 +0100

    installed mongoose - setup new db connection - added env file

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+require('dotenv').config();
+require('./config/db.js')();
+
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/views/'));
 
